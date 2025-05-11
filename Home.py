c167131d2d0d86f2e5fabee5ab324c4b0b0cee6c
Tanu Agarwal <116576884+tanu100894@users.noreply.github.com>
2025-05-11 14:31:27 +0200

    Minor Changes

--- Home.py
+++ Home.py
@@ -3,6 +3,8 @@ import functions
 
 
 todos = functions.get_todos()
+st.set_page_config(layout="wide")
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -10,7 +12,11 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase the productivity.")
+st.write("This app is to increase the <b>productivity</b>.",
+         unsafe_allow_html=True)
+
+st.text_input(label=" ", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -20,5 +26,3 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.rerun()
 
-st.text_input(label=" ", placeholder="Add new todo...",
-              on_change=add_todo, key='new_todo')
